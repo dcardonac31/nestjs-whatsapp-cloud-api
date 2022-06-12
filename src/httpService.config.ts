4efb31e7d0ba970d0d220e8c2e20c0c019a71d39
David Sneider Cardona Cardenas <davidec10@gmail.com>
2022-06-12 12:44:41 -0500

    Update token

--- src/httpService.config.ts
+++ src/httpService.config.ts
@@ -6,7 +6,7 @@ export class HttpConfigService implements HttpModuleOptionsFactory {
     createHttpOptions(): HttpModuleOptions {
         return {
             headers: {
-                'Authorization': 'Bearer EAAE225RZA0TwBANagJoZC41nf1KmDyGe7yIZCovMnhZCvWaetoo93UuTtsdcypMGjCuIAhjZCZCo4uHGPI5Ix5pgBts06IpFI0S0byA2pTTD4GZCv1tFdopoJ09HYaklLrHAg5KOsUTdXCB4snGac9eNGRDtpn3jnG8v9nty33JMNTVKXLuAZAjoGbVdZAxexUq7SnHU66j7txWrGM3uTgoUDZAeyFZAB3ZBR24ZD',
+                'Authorization': 'Bearer EAAE225RZA0TwBAJFMbqpXDttb0ZCLxKTweNVKgDqtRuJM51Cn8JNaylDAcyJJSpEqf8W3ZCHsm2pn8caO0uUcMQTuk44z0701OjdAwwKvIU6fDSSeYqAilZC8FtD4PZCVWwAmaLJZAhkmPIpBhAl1lMxXECUvRtT5uRS0oILPZBM7UzT5N4RBxgT3v0pK1dQvKVcOVwSI8LejVtKNfMtIyJiqNt58F9oe8ZD',
                 'Content-Type': 'application/json'
             }
         }

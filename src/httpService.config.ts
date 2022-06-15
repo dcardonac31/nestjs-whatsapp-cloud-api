12749777081513e4c58b404b7051c003e080b821
David Sneider Cardona Cardenas <davidec10@gmail.com>
2022-06-14 23:39:48 -0500

    Nuevo token

--- src/httpService.config.ts
+++ src/httpService.config.ts
@@ -6,7 +6,7 @@ export class HttpConfigService implements HttpModuleOptionsFactory {
     createHttpOptions(): HttpModuleOptions {
         return {
             headers: {
-                'Authorization': 'Bearer EAAE225RZA0TwBAMxijsTuGjZCm1nSENSVZBKVWTtQifl0dozNfg50AqlAZBbkhZBBMQMgy5QC6wkEZCB0npzALOr8Qs0gRi6JbSOScnnvm0HOVLEqDOGFQzZBWwqhgqAjkZBdMyOypKIJF0tSn2yYMUDyPMFrIoihoQtenB42ehuMWo27oVIbjFlZBz15R2HwWDjIavQYO06jESchyMYyRGqZB3kZB9DjH7YZCEZD',
+                'Authorization': 'Bearer TokenPruebaCaducaCada24Horas',
                 'Content-Type': 'application/json'
             }
         }
